59e350ad097d025395d3637dc45f55babfe21f54
GreatTusk <fern.belmar@duocuc.cl>
2025-02-03 22:23:25 -0300

    added build-logic

--- composeApp/src/commonMain/kotlin/com/f776/App.kt
+++ composeApp/src/commonMain/kotlin/com/f776/App.kt
@@ -6,7 +6,6 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
-import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
 import androidx.compose.runtime.Composable
@@ -16,7 +15,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.window.core.layout.WindowWidthSizeClass
 import com.f776.ui.theme.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
